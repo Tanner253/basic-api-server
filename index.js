a83375927843d9911ce1777b9d6d85763b6db593
Tanner253 <PercivalTanner@gmail.com>
2022-03-23 18:42:27 -0700

    missed start

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 'use strict';
 const { sequelize, peopleModel, musicModel } = require('./collection');
-
+const { start } = require('./server/server.js');
 
 sequelize.sync()
 .then( () => {
   console.log('database is synced and ready to go');
-  peopleModel.create({name: 'tom'});
-  musicModel.create({songName: 'brap brap boop'})
+  // peopleModel.create({name: 'tom'});
+  // musicModel.create({songName: 'brap brap boop'});
+  start();
 })
 .catch(err => {
   console.error(err);
 })
+
